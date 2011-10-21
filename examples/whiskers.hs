5473a057b36a43498ddc5deff1aedbd16a4ff293
Bas van Dijk <v.dijk.bas@gmail.com>
2011-10-21 13:02:49 +0200

    Fix example

--- examples/whiskers.hs
+++ examples/whiskers.hs
@@ -5,22 +5,23 @@ import Text.RSS
 import Data.Maybe
 import Network.URI
 import System.IO.Unsafe
-import System.Time
+import Data.Time.Clock
 
-main = putStrLn rssTest
+main = putStrLn . showXML . rssToXML . rss =<< getCurrentTime
 
--- A simple static test feed
-rssTest =   showXML $ rssToXML (RSS "my whiskers" 
-				(fromJust (parseURI "http://www.n-heptane.com")) 
-				"they are very pointy and luxurious" 
-				[] 
-				[[ Title "yea-haw"
-				 , Link (fromJust (parseURI "http://www.n-heptane.com/"))
-				 , Description "the best site ever!!!"
-				 , Author "jeremy@n-heptane.com (Jeremy Shaw)"
-				 , Category Nothing "meow"
-				 , Enclosure (fromJust (parseURI "http://www.n-heptane.com/newpics/alice.gif")) 7333 "image/jpeg"
-				 , Guid "whee babayyyy!"
-				 , PubDate (unsafePerformIO (getClockTime >>= toCalendarTime))
-				 , Source (fromJust (parseURI "http://www.google.com/")) "The best search engine eva!"
-				]])
+rss :: UTCTime -> RSS
+rss t = RSS "my whiskers"
+            (fromJust (parseURI "http://www.n-heptane.com"))
+            "they are very pointy and luxurious"
+            []
+            [[ Title "yea-haw"
+             , Link (fromJust (parseURI "http://www.n-heptane.com/"))
+             , Description "the best site ever!!!"
+             , Author "jeremy@n-heptane.com (Jeremy Shaw)"
+             , Category Nothing "meow"
+             , Enclosure (fromJust (parseURI "http://www.n-heptane.com/newpics/alice.gif")) 7333 "image/jpeg"
+             , Guid True "whee babayyyy!"
+             , PubDate t
+             , Source (fromJust (parseURI "http://www.google.com/")) "The best search engine eva!"
+             ]
+            ]
