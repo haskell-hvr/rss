8dcf11b62a2110811948106389786f14906f89b8
Herbert Valerio Riedel <hvr@gnu.org>
2018-10-06 00:50:02 +0200

    Refactor .cabal file
    This also addresses #7 by switching from Safe to Trustworthy level
    as not all dependencies we depend upon weren't SafeHaskell-clean.

--- Text/RSS.hs
+++ Text/RSS.hs
@@ -1,9 +1,5 @@
 {-# LANGUAGE CPP #-}
 
-#if __GLASGOW_HASKELL__ >= 704
-{-# LANGUAGE Safe #-}
-#endif
-
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Text.RSS
